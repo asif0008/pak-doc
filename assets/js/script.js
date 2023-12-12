2d2b2a565b3dc7da444a26fbebe45e01cfedd0be
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2023-12-12 15:00:17 +0500

    carousel section added

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,31 @@ document.addEventListener('DOMContentLoaded', function(){
 
     var typed = new Typed('#typed-output', options);
   });
+
+//   Swiper js
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    centeredSlides: false,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    breakpoints: {
+      769: {
+        slidesPerView: 4.2,
+        slidesPerGroup: 4.2,
+      },
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
