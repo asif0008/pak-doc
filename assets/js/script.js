282267fedef759392c1260c24d24598d4ec54a54
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2023-12-14 14:03:17 +0500

    css update, header update, navbar update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,19 @@ let responsive_header = document.getElementById('side-nav');
 let closeside_nav = document.getElementById('close-side-nav');
 hamburger_menu.addEventListener("click", () => {
     responsive_header.classList.add('active');
+    document.body.style.backgroundColor = 'rgba(42, 42, 42, 0.90)';
 
 })
 closeside_nav.addEventListener('click', () => {
     responsive_header.classList.remove('active');
+    document.body.style.backgroundColor = 'unset';
 });
 
 //   Swiper js
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1.2,
     centeredSlides: false,
-    slidesPerGroupSkip: 1,
+    slidesPerGroupSkip: 0,
     grabCursor: true,
     keyboard: {
       enabled: true,
@@ -23,22 +25,27 @@ var swiper = new Swiper(".mySwiper", {
       1024: {
         slidesPerView: 4.2,
         slidesPerGroup: 4.2,
+        slidesPerGroupSkip: 0,
       },
       991: {
         slidesPerView: 3.7,
         slidesPerGroup: 4.2,
+        slidesPerGroupSkip: 0,
       },
       769: {
         slidesPerView: 3.2,
         slidesPerGroup: 4.2,
+        slidesPerGroupSkip: 0,
       },
       576: {
         slidesPerView: 2.2,
         slidesPerGroup: 4.2,
+        slidesPerGroupSkip: 0,
       },
     },
     scrollbar: {
       el: ".swiper-scrollbar",
+      draggable: true,
     },
     navigation: {
       nextEl: ".swiper-button-next",
@@ -54,7 +61,7 @@ var swiper = new Swiper(".mySwiper", {
 var swiper = new Swiper(".topSearchedSwiper", {
     slidesPerView: 3.7,
     centeredSlides: false,
-    slidesPerGroupSkip: 1,
+    slidesPerGroupSkip: 0,
     grabCursor: true,
     keyboard: {
       enabled: true,
